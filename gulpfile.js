ff5d9996cb44cc67c1b596287dbe7cfbe78dd04a
Rodrigo Bergenthal <Rodrigo_bergenthal@live.com>
2024-06-05 08:29:57 -0300

    add funções

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+function funcaoPadrao(callback) {
+  console.log("Executando via GULP");
+  callback();
+}
+
+function dizoi (callback) {
+  console.log("ola Gulp");
+  callback();
+  diztchau();
+}
+
+function diztchau (){
+  console.log("Tchau gulp");
+}
+
+
+exports.default = funcaoPadrao;
+exports.dizoi = dizoi;
